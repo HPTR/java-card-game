27382eaa94df93c53b4d7d0c99b3c728f46f2d7e
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-05 12:54:44 +0100

    Add automatic Snap game functionality

--- src/Snap.java
+++ src/Snap.java
@@ -1,10 +1,23 @@
+import java.util.Objects;
+
 public class Snap extends CardGame {
 
     public static void main() {
-     CardGame.shuffleDeck();
-     Card currentCard = CardGame.dealCard();
-     String previousSymbol = currentCard.getSymbol();
-     printMessage(currentCard.toString());
+        CardGame.shuffleDeck();
+        String previousSymbol = "";
+
+        boolean isActive = true;
+        while (isActive) {
+            Card currentCard = CardGame.dealCard();
+            printMessage(currentCard.toString());
+            if (Objects.equals(currentCard.getSymbol(), previousSymbol)) {
+                printMessage("Snap!");
+                isActive = false;
+            } else {
+                previousSymbol = currentCard.getSymbol();
+            }
+        }
+
     }
 
 }
