f04810a09a37ab1f459927e8a5b54e878a5ef29b
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-05 11:15:35 +0100

    Update deck generation and add printCards method

--- src/CardGame.java
+++ src/CardGame.java
@@ -2,31 +2,26 @@ import java.util.ArrayList;
 
 public class CardGame {
 
-    ArrayList<Card> deckOfCards = new ArrayList<>();
+    static ArrayList<Card> deckOfCards = new ArrayList<>();
     
     static {
-        for (int i = 0; i < 3; i++) {
-            String suit = "";
+        for (int i = 0; i < 4; i++) {
 
-            switch (i) {
-                case 0:
-                    suit = "spade";
-                    break;
-                case 1:
-                    suit = "club";
-                    break;
-                case 2:
-                    suit = "diamond";
-                    break;
-                case 3:
-                    suit = "heart";
-                    break;
-                default:
-                    System.out.println("This should never happen");
-            }
+            String[] suits = new String[] {"\u2664;", "\u2661", "\u2667", "\u2662"};
+
             for (int j = 0; j < 13; j++) {
-                deckOfCards.add(new Card(suit, ""))
+
+                String[] symbols = new String[] {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+                Integer[] values = new Integer[] {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+                deckOfCards.add(new Card(suits[i], symbols[j], values[j]));
             }
+        }
+    }
+
+    public static void printCards() {
+        for (Card card : deckOfCards) {
+            System.out.println(card.toString());
         }
     }
 
