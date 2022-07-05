69680f5c69805a2e975bbbe0c17b7de94e94c997
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-05 11:52:32 +0100

    Add symbol and value arrays for card generation

--- src/CardGame.java
+++ src/CardGame.java
@@ -11,8 +11,8 @@ public class CardGame {
 
             for (int j = 0; j < 13; j++) {
 
-                String[] symbols = new String[] {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-                Integer[] values = new Integer[] {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+                String[] symbols = new String[]{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+                Integer[] values = new Integer[]{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
                 deckOfCards.add(new Card(suits[i], symbols[j], values[j]));
             }
