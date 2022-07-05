8eee2676c367865619821961010d6e44563ce48f
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-05 12:34:43 +0100

    Add main method to Snap and call in main

--- src/Snap.java
+++ src/Snap.java
@@ -1,5 +1,10 @@
 public class Snap extends CardGame {
 
-
+    public static void main() {
+     CardGame.shuffleDeck();
+     Card currentCard = CardGame.dealCard();
+     String previousSymbol = currentCard.getSymbol();
+     printMessage(currentCard.toString());
+    }
 
 }
