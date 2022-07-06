ae3268da3590e32300d4b045a383babf5febe8cc
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-06 12:32:12 +0100

    Add commands based navigation to main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        Snap.main();
+
+        Commands currentCommands = new HomeCommands();
+        currentCommands.run();
+
+        boolean isActive = true;
+        while(isActive){
+            switch(currentCommands.getNextCommands()){
+                case "onePlayer" :
+                    currentCommands = new PlayerCommands();
+                    currentCommands.run();
+                    Player player = ((PlayerCommands) currentCommands).getUser();
+
+                    Snap.onePlayer(player);
+                    CardGame.printMessage(player.toString());
+                    break;
+
+                case "twoPlayer" :
+                    currentCommands = new PlayerCommands();
+                    currentCommands.run();
+                    Player playerOne = ((PlayerCommands) currentCommands).getUser();
+
+                    currentCommands = new PlayerCommands();
+                    currentCommands.run();
+                    Player playerTwo = ((PlayerCommands) currentCommands).getUser();
+
+                    Snap.twoPlayer(playerOne, playerTwo);
+                    CardGame.printMessage(playerOne.toString());
+                    CardGame.printMessage(playerTwo.toString());
+                    break;
+
+                default:
+                    isActive = false;
+            }
+        }
     }
 }
