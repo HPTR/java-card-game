665d6ba530899b9a1d9989a9baf8f80390f2c0dd
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-06 12:33:04 +0100

    Add two different methods to play the game with one or two players

--- src/Snap.java
+++ src/Snap.java
@@ -1,22 +1,72 @@
 import java.util.Objects;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Snap extends CardGame {
 
-    public static void main() {
+    static Timer timer = new Timer();
+
+    public static void getInput() {
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+    }
+
+    public static void onePlayer(Player player) {
         CardGame.shuffleDeck();
         String previousSymbol = "";
-
         boolean isActive = true;
         while (isActive) {
             Card currentCard = CardGame.dealCard();
             assert currentCard != null;
             printMessage(currentCard.toString());
+            getInput();
             if (Objects.equals(currentCard.getSymbol(), previousSymbol)) {
-                printMessage("Snap!");
+                printMessage("Snap!\n");
+                player.incrementScore();
                 isActive = false;
             } else {
                 previousSymbol = currentCard.getSymbol();
             }
+        }
+    }
+
+    public static void startTimer() {
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        })
+    }
+
+    public static void twoPlayer(Player playerOne, Player playerTwo) {
+        CardGame.shuffleDeck();
+        String previousSymbol = "";
+        boolean isActive = true;
+        int turnCounter = 1;
+        while (isActive) {
+            Card currentCard = CardGame.dealCard();
+            assert currentCard != null;
+            printMessage(currentCard.toString());
+            if (Objects.equals(currentCard.getSymbol(), previousSymbol)) {
+                printMessage("\nSnap!\n");
+
+                if (turnCounter % 2 == 0) {
+                    playerTwo.incrementScore();
+                    printMessage(String.format("%s wins!", playerTwo.getName()));
+                } else {
+                    playerOne.incrementScore();
+                    printMessage(String.format("%s wins!", playerOne.getName()));
+                }
+
+                isActive = false;
+            } else {
+                getInput();
+                previousSymbol = currentCard.getSymbol();
+                turnCounter += 1;
+            }
+
         }
 
     }
