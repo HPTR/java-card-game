a00f347f0ee513146a7f2b4537e6afcddf340e05
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-12 10:49:14 +0100

    Update Snap game calls to match changes to the class construction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,32 +3,34 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-            Commands currentCommands = new HomeCommands();
-            currentCommands.run();
+        Commands currentCommands = new HomeCommands();
+        currentCommands.run();
 
-            switch(currentCommands.getNextCommands()){
-                case "onePlayer" :
-                    currentCommands = new PlayerCommands();
-                    currentCommands.run();
-                    Player player = ((PlayerCommands) currentCommands).getUser();
+        switch(currentCommands.getNextCommands()){
+            case "onePlayer" :
+                currentCommands = new PlayerCommands();
+                currentCommands.run();
+                Player player = ((PlayerCommands) currentCommands).getUser();
 
-                    Snap.onePlayer(player);
-                    break;
+                Snap snapGame = new Snap(player, null);
+                snapGame.onePlayer();
+                break;
 
-                case "twoPlayer" :
-                    currentCommands = new PlayerCommands();
-                    currentCommands.run();
-                    Player playerOne = ((PlayerCommands) currentCommands).getUser();
+            case "twoPlayer" :
+                currentCommands = new PlayerCommands();
+                currentCommands.run();
+                Player playerOne = ((PlayerCommands) currentCommands).getUser();
 
-                    currentCommands = new PlayerCommands();
-                    currentCommands.run();
-                    Player playerTwo = ((PlayerCommands) currentCommands).getUser();
+                currentCommands = new PlayerCommands();
+                currentCommands.run();
+                Player playerTwo = ((PlayerCommands) currentCommands).getUser();
 
-                    Snap.twoPlayer(playerOne, playerTwo);
-                    break;
+                snapGame = new Snap(playerOne, playerTwo);
+                snapGame.twoPlayer();
+                break;
 
-                default:
-                    System.exit(0);
-            }
+            default:
+                System.exit(0);
+        }
     }
 }
